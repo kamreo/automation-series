cd39710ef3935cba63d9f091016e5ed200c2040c
kamreo <kamciu9@gmail.com>
2022-04-13 00:15:38 +0200

    little bot fix

--- ig.py
+++ ig.py
@@ -13,16 +13,14 @@ class Ig:
     def __init__(self, ig_login, ig_password):
         self.login = ig_login
         self.password = ig_password
+        self.bot = Bot()
         
     def get_ig_posts(self):
-        self.bot = Bot()
-        sleep(2)
         self.bot.login(username = self.login, password = self.password)
         sleep(2)
         return self.bot.get_your_medias()
 
     def create_ig_post(self, title, photo, videoUrl):
-        self.bot = Bot()
         self.bot.login(username = self.login, password = self.password)
         sleep(4)
         self.bot.upload_photo(photo, caption="New video "+ title + "uploaded on my channel. You can watch it here: " + videoUrl + "!")
